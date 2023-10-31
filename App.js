f183b3aa71ce95333d3c35987f49831e561fdd5d
plgdz <40022026+plgdz@users.noreply.github.com>
2023-10-31 17:53:25 -0400

    cd

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+// Composant fonctionnel
+
+// function Chanson() {
+//     const nom  = 'La Puissance'
+//     return (
+//         <View>
+//             <Text>{nom}</Text>
+//         </View>
+//     )
+// }
+
+
+// Composant en arrow function
+
+// const Chanson = () => {
+//     const nom  = 'La Puissance'
+//     return (
+//         <View>
+//             <Text>{nom}</Text>
+//         </View>
+//     )
+// }
+
+
+// Composant class
+
+class Chanson extends React.Component{
+
+    render() {
+        const nom = "Rohff"
+        return (
+            <View style={style.viewAppearance} >
+                <Text>{nom}</Text>
+            </View>
+        )
+    }
+}
+
+const style = StyleSheet.create(
+    {
+        viewAppearance : {
+            backgroundColor: 'red',
+            borderRadius: 8,
+            margin: 10
+        }
+    }
+)
+
+export default Chanson
